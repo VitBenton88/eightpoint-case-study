121707a6dda7e6e7345ccc0032622cd5357fab8c
Vit Benton <Vit@vitbenton.com>
2025-06-19 19:49:15 -0400

    varous layout updates for news feed

--- components/NewsArticle.vue
+++ components/NewsArticle.vue
@@ -1,4 +1,6 @@
 <script setup>
+import AnchorLink from './AnchorLink.vue';
+
 defineProps({
   article: {
     type: Object,
@@ -12,14 +14,47 @@ defineProps({
     }
   }
 })
+
+const truncateHeading = text => {
+  const maxLength = 40
+
+  if (!text) return ''
+  return text.length > maxLength
+    ? text.slice(0, maxLength).trim() + '...'
+    : text
+}
 </script>
 
 <template>
   <article>
     <img :src="article.image" :alt="article.title">
-    <h2>{{ article.title }}</h2>
-    <p>{{ article.source }}</p>
-    <p v-if="article.tag">{{ article.tag }}</p>
-    <p>{{ article.timestamp }}</p>
+    <div class="details">
+      <AnchorLink :href="article.href" target="_blank" :aria-label="article.title">
+        <h2>{{ truncateHeading(article.title) }}</h2>
+      </AnchorLink>
+      <p>{{ article.source }}</p>
+      <p v-if="article.tag">{{ article.tag }}</p>
+      <p>{{ article.timestamp }}</p>
+    </div>
   </article>
 </template>
+
+<style scoped>
+article {
+  border: solid 1px #ddd;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.details {
+  padding: 0 .5em;
+}
+
+img {
+  width: 100%;
+}
+
+h2 {
+  font-size: 16px;
+}
+</style>
--- components/NewsFeed.vue
+++ components/NewsFeed.vue
@@ -10,15 +10,27 @@ defineProps({
 </script>
 
 <template>
-  <ul>
-    <li v-for="item in items" :key="item.id">
-      <NewsArticle :article="item" />
-    </li>
-  </ul>
+  <div class="feed">
+    <ul>
+      <li v-for="item in items" :key="item.id">
+        <NewsArticle :article="item" />
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
+.feed {
+  max-width: 900px;
+  width: 100%;
+}
+
 ul {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(calc(190px - 1rem), 1fr));
+  gap: 1rem;
   list-style: none;
+  margin: 0;
+  padding: 0;
 }
 </style>
