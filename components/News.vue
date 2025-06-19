3754499e5c182a72f3e87571900559cef9c60b94
Vit Benton <Vit@vitbenton.com>
2025-06-19 18:09:21 -0400

    News component with tabs and feed created

---
+++ components/News.vue
@@ -0,0 +1,36 @@
+<script setup>
+import { ref, watchEffect } from 'vue'
+import { useAsyncData } from '#app'
+import Feed from '@/components/Feed.vue'
+import TabNav from '@/components/TabNav.vue'
+
+const tabs = [
+  { id: 'home', label: 'Home' },
+  { id: 'news', label: 'News' },
+  { id: 'sports', label: 'Sports' },
+  { id: 'money', label: 'Money' },
+  { id: 'lifestyle', label: 'Lifestyle' },
+  { id: 'politics', label: 'Politics' },
+  { id: 'health', label: 'Health' },
+  { id: 'food', label: 'Food' },
+  { id: 'entertainment', label: 'Entertainment' },
+]
+
+const selectedTab = ref('home')
+const emit = defineEmits(['update:selectedTab'])
+
+const { data: articles, refresh } = await useAsyncData(
+  'articles',
+  () => $fetch('/api/news', {
+    query: { category: selectedTab.value }
+  }),
+  { watch: [selectedTab] }
+)
+
+const selectTab = tabId => selectedTab.value = tabId
+</script>
+
+<template>
+  <TabNav :tabs :selectedTab @selectTab="selectTab" />
+  <Feed :items="articles" />
+</template>
