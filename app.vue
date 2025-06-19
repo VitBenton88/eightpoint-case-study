8d94a48eeb0eef1bc3c78857eeeb970e33e06e82
Vit Benton <Vit@vitbenton.com>
2025-06-19 11:52:15 -0400

    SearchInput component usage

--- app.vue
+++ app.vue
@@ -1,6 +1,21 @@
 <template>
-  <div>
+  <main>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+    <div class="search">
+      <SearchInput />
+    </div>
+  </main>
 </template>
+
+<style scoped>
+main {
+  display: flex;
+  justify-content: center;
+  padding: 1rem;
+  width: 100%;
+}
+
+.search {
+  padding: 1rem;
+}
+</style>
