4473901e1e9765e86325d19d7c340ad9a967a1df
Vit Benton <Vit@vitbenton.com>
2025-06-19 18:07:59 -0400

    base feed component created

---
+++ components/Feed.vue
@@ -0,0 +1,21 @@
+<template>
+  <ul>
+    <li v-for="item in items" :key="item.id">
+      <article>
+        <h2>{{ item.title }}</h2>
+        <p>{{ item.source }}</p>
+        <p v-if="item.tag">{{ item.tag }}</p>
+        <p>{{ item.timestamp }}</p>
+      </article>
+    </li>
+  </ul>
+</template>
+
+<script setup>
+const props = defineProps({
+  items: {
+    type: Array,
+    required: true
+  }
+})
+</script>
