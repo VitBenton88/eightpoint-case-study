aee2871746fc52138a26866a78e676ce2cdded0a
Vit Benton <Vit@vitbenton.com>
2025-06-19 18:09:07 -0400

    tabbed navigation component created

---
+++ components/TabNav.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tabs">
+    <nav class="tab-buttons">
+      <button v-for="(tab, index) in tabs" :key="index" :class="{ active: selectedTab === tab.id }"
+        @click="handleClick(tab.id)">
+        {{ tab.label }}
+      </button>
+    </nav>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  selectedTab: {
+    required: true
+  },
+  tabs: {
+    type: Array,
+    required: true
+  }
+})
+
+const emit = defineEmits(['selectTab']);
+
+const handleClick = tab => emit('selectTab', tab)
+</script>
+
+<style scoped>
+.tab-buttons {
+  display: flex;
+  gap: 0.5rem;
+  margin-top: 5rem;
+}
+
+.tab-buttons button {
+  padding: 0.5rem 1rem;
+  background: #eee;
+  border: none;
+  cursor: pointer;
+}
+
+.tab-buttons button.active {
+  background: #333;
+  color: white;
+}
+
+.tab-content {
+  padding: 1rem;
+  border: 1px solid #ccc;
+}
+</style>
