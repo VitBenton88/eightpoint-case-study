9b33926ecdd44b73799134264739bfd66b800e3f
Vit Benton <Vit@vitbenton.com>
2025-06-20 08:35:43 -0400

    custom spotify icon installed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon']
+  modules: ['@nuxt/eslint', '@nuxt/icon'],
+  icon: {
+    customCollections: [
+      {
+        prefix: 'icons',
+        dir: './assets/icons'
+      },
+    ],
+  },
 })
