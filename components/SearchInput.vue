5a0bdb729ecc596a8aa3000fcaf943dc91370cd2
Vit Benton <Vit@vitbenton.com>
2025-06-19 20:00:24 -0400

    search input bkg white

--- components/SearchInput.vue
+++ components/SearchInput.vue
@@ -22,6 +22,7 @@ const handleSubmit = () => {
 
 <style scoped>
 form {
+  background: white;
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
   border: 1px solid #666;
   border-radius: 25px;
